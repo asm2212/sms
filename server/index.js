5a873645de740ac764b9ad67a699a4b28898a3c2
asm2212 <asmareadmasu0@gmail.com>
2024-05-02 08:57:47 +0300

    add everthin to server side

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 
 const app = express()
-// const Routes = require("./routes/route.js")
+const Routes = require("./routes/route.js")
 
 const PORT = process.env.PORT || 5000
 
@@ -20,7 +20,7 @@ mongoose
     .then(console.log("Connected to MongoDB"))
     .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
 
-// app.use('/', Routes);
+app.use('/', Routes);
 
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`)
